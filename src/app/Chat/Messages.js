3dcaf9b522ce3e9488e340a8f3f9a738e9bd2b01
emil <poolsar.42@gmail.com>
2021-11-18 00:34:03 +0300

    fix: minor bug fixed

--- src/app/Chat/Messages.js
+++ src/app/Chat/Messages.js
@@ -1,15 +1,62 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+
+import { makeFileObject } from "../Utils/filemaker";
+//* web3.storage stuff
+import {
+  storeWithProgress,
+  fetchHistory,
+} from "../../decent_network/web3Storage";
 
 //* we only can upload files to web3.storage
 //* this will convert .json to File
 
-import { useMessages, useMessage } from "../Hooks/messagesHooks";
-
 const Messages = ({ channel, ipfs, message, setPeers }) => {
   //* List of all messages
-  const [messages, setMessages] = useMessages(channel);
+  const [messages, setMessages] = useState([]);
 
-  useMessage(message, ipfs, messages, setMessages, setPeers, channel);
+  useEffect(() => {
+    (async () => {
+      console.log("changes channel", await fetchHistory());
+      setMessages(
+        (await fetchHistory()).filter((message) => message.channel === channel)
+      );
+    })();
+  }, []);
+
+  useEffect(() => {
+    (async () => {
+      console.log("changes channel", await fetchHistory());
+      setMessages(
+        (await fetchHistory()).filter((message) => message.channel === channel)
+      );
+    })();
+  }, [channel]);
+
+  useEffect(() => {
+    (async () => {
+      if (message.message && message.channel === channel) {
+        setMessages([
+          ...messages,
+          {
+            message: message.message,
+            username: message.username,
+            channel: message.channel,
+            color: message.color,
+          },
+        ]);
+        //* I know - it's bad sync all peers every time message is thrown
+        //* It's just for now
+        //* It will not display you on your end (idk why)
+        setPeers(await ipfs.pubsub.peers("example_topic"));
+
+        //* Upload history to web3.storage
+        const _messages = await fetchHistory();
+        _messages.push(message);
+        const file = makeFileObject(_messages);
+        await storeWithProgress([file]);
+      }
+    })();
+  }, [message]);
 
   return (
     <div>
